1c2257d312e9916ec0c28cb8e6981c72e2ee99ec
Grant Wilkins <grant.wilkins371@gmail.com>
2020-06-25 09:51:17 -0400

    Update rapl_plot.c

--- rapl_plot/rapl_plot.c
+++ rapl_plot/rapl_plot.c
@@ -154,7 +154,7 @@ int main (int argc, char **argv)
      int control;
      control = 0;
      while(1) {
-	if(control == 300){ // stop after 30 seconds
+	if(control == 300000){ // stop after 30 seconds
 		printf("YOU ARE TAKING TOO LONG ... QUIT");
 		exit(1);
 	}
